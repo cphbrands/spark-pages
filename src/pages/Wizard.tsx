8eb9b883818e5dfcf526bfac90d4f73930e88ddd
Hamed <cphbrands@hotmail.com>
2026-01-01 14:49:03 +0100

    Fix wizard page crash (remove undefined banner)

--- src/pages/Wizard.tsx
+++ src/pages/Wizard.tsx
@@ -228,8 +228,6 @@ export default function Wizard() {
         </div>
       </header>
 
-      <BuilderTopBanner />
-
       <main className="max-w-6xl mx-auto px-6 py-8 space-y-8">
         <div className="grid grid-cols-1 md:grid-cols-5 gap-3">
           {steps.map((step, idx) => (
